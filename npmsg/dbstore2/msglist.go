df9f7c3078204448e525608e3970d9115aa715af
jayven <kassarar@gmail.com>
2018-11-17 18:03:07 +0800

    Add msgList

---
+++ npmsg/dbstore2/msglist.go
@@ -0,0 +1,108 @@
+package dbstore
+
+import (
+	"sync"
+)
+
+var (
+	msgNodePool = &sync.Pool{
+		New: func() interface{} {
+			return &msgNode{}
+		},
+	}
+)
+
+var (
+	newNode    = func() *msgNode { return msgNodePool.Get().(*msgNode) }
+	deleteNode = func(node *msgNode) { msgNodePool.Put(node) }
+)
+
+// msgList is a list of messages.
+type msgList struct {
+	head msgNode // head.next is the first item and head.prev is the last item.
+}
+
+// msgNode is a single message. It belong to only one msgList at any time.
+type msgNode struct {
+	id      uint64
+	subject string
+	data    []byte
+	prev    *msgNode
+	next    *msgNode
+}
+
+// newMsgList creates an empty msgList.
+func newMsgList() *msgList {
+	ret := &msgList{}
+	head := &ret.head
+	head.prev = head
+	head.next = head
+	return ret
+}
+
+// NewNode creates a new msgNode and appends it to list.
+func (list *msgList) NewNode(id uint64, subject string, data []byte) *msgNode {
+	ret := newNode()
+	ret.id = id
+	ret.subject = subject
+	ret.data = data
+	ret.attach(list)
+	return ret
+}
+
+// AppendNode detaches a msgNode from its current owning list and appends to list.
+func (list *msgList) AppendNode(node *msgNode) {
+	node.detach()
+	node.attach(list)
+}
+
+// Reset deletes all msgNode in a msgList.
+func (list *msgList) Reset() {
+	head := &list.head
+	node := head.next
+	for node != head {
+		next := node.next
+		deleteNode(node)
+		node = next
+	}
+	head.next = head
+	head.prev = head
+}
+
+// MigrateTo migrates all msgNode from list to target.
+func (list *msgList) MigrateTo(target *msgList) {
+	head := &list.head
+	if head.next == head {
+		return
+	}
+	first := head.next
+	last := head.prev
+	first.prev = &target.head
+	last.next = &target.head
+	target.head.next = first
+	target.head.prev = last
+
+	head.next = head
+	head.prev = head
+}
+
+func (node *msgNode) attach(list *msgList) {
+	head := &list.head
+	node.prev = head.prev
+	node.next = head
+	head.prev.next = node
+	head.prev = node
+}
+
+func (node *msgNode) detach() {
+	prev := node.prev
+	next := node.next
+	if prev != nil {
+		prev.next = next
+	}
+	if next != nil {
+		next.prev = prev
+	}
+	node.prev = nil
+	node.next = nil
+}
---
+++ npmsg/dbstore2/msglist_test.go
@@ -0,0 +1,99 @@
+package dbstore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMsgList(t *testing.T) {
+	assert := assert.New(t)
+	origDeleteNode := deleteNode
+	defer func() {
+		deleteNode = origDeleteNode
+	}()
+	deleteNodeC := make(chan uint64, 10000)
+	deleteNode = func(node *msgNode) {
+		deleteNodeC <- node.id
+		origDeleteNode(node)
+	}
+	expectDeleteNode := func(expectId uint64) {
+		id := <-deleteNodeC
+		assert.Equal(id, expectId)
+	}
+
+	// --- test NewNode ---
+	l1 := newMsgList()
+	{
+		assert.Equal(&l1.head, l1.head.prev)
+		assert.Equal(&l1.head, l1.head.next)
+	}
+
+	n1 := l1.NewNode(1, "sub1", nil)
+	{
+		assert.Equal(n1, l1.head.next) // First item is n1.
+		assert.Equal(n1, l1.head.prev) // Last item is n1.
+		assert.Equal(&l1.head, n1.prev)
+		assert.Equal(&l1.head, n1.next)
+	}
+	n2 := l1.NewNode(2, "sub2", nil)
+	{
+		assert.Equal(n1, l1.head.next) // First item is n1.
+		assert.Equal(n2, l1.head.prev) // Last item is n2.
+		assert.Equal(&l1.head, n1.prev)
+		assert.Equal(n2, n1.next)
+		assert.Equal(n1, n2.prev)
+		assert.Equal(&l1.head, n2.next)
+	}
+
+	// --- test AppendNode ---
+	l2 := newMsgList()
+
+	l2.AppendNode(n1)
+	{
+		assert.Equal(n2, l1.head.next) // First item is n2.
+		assert.Equal(n2, l1.head.prev) // Last item is n2.
+		assert.Equal(&l1.head, n2.prev)
+		assert.Equal(&l1.head, n2.next)
+		assert.Equal(n1, l2.head.next) // First item is n1.
+		assert.Equal(n1, l2.head.prev) // Last item is n1.
+		assert.Equal(&l2.head, n1.prev)
+		assert.Equal(&l2.head, n1.next)
+	}
+
+	l2.AppendNode(n2)
+	{
+		assert.Equal(&l1.head, l1.head.next) // l1 is empty now.
+		assert.Equal(&l1.head, l1.head.prev) // l1 is empty now.
+		assert.Equal(n1, l2.head.next)       // First item is n1.
+		assert.Equal(n2, l2.head.prev)       // Last item is n2.
+		assert.Equal(&l2.head, n1.prev)
+		assert.Equal(n2, n1.next)
+		assert.Equal(n1, n2.prev)
+		assert.Equal(&l2.head, n2.next)
+	}
+
+	// --- test MigrateTo ---
+	l2.MigrateTo(l1)
+	{
+		assert.Equal(&l2.head, l2.head.next) // l2 is empty now.
+		assert.Equal(&l2.head, l2.head.prev) // l2 is empty now.
+		assert.Equal(n1, l1.head.next)       // First item is n1.
+		assert.Equal(n2, l1.head.prev)       // Last item is n2.
+		assert.Equal(&l1.head, n1.prev)
+		assert.Equal(n2, n1.next)
+		assert.Equal(n1, n2.prev)
+		assert.Equal(&l1.head, n2.next)
+	}
+
+	// --- test Reset ---
+	l1.Reset()
+	{
+		expectDeleteNode(1)
+		expectDeleteNode(2)
+		assert.Len(deleteNodeC, 0)
+		assert.Equal(&l1.head, l1.head.next) // l1 is empty now.
+		assert.Equal(&l1.head, l1.head.prev) // l1 is empty now.
+	}
+
+}
