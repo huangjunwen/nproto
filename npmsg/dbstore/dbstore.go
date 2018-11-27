55887d146da1c8afb66c5f938a9f82faf51d9d53
jayven <kassarar@gmail.com>
2018-11-27 11:31:45 +0800

    Replace old implementation

--- npmsg/dbstore/dbstore.go
+++ npmsg/dbstore/dbstore.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"sync"
-	"time"
 
 	"github.com/huangjunwen/nproto/npmsg"
 
@@ -14,26 +14,29 @@ import (
 )
 
 var (
-	DefaultRetryWait = 2 * time.Second
-	DefaultFlushWait = 15 * time.Second
-	DefaultBatch     = 1000
+	DefaultDeleteBulkSize = 100
+	DefaultMaxInflight    = 2048
+	DefaultMaxBuf         = 512
 )
 
 var (
-	ErrClosed = errors.New("nproto.npmsg.dbstore.DBMsgStore: Closed.")
+	ErrMaxInflightAndBuf = errors.New("nproto.npmsg.dbstore.DBStore: MaxBuf should be <= MaxInflight")
+	ErrUnknownDialect    = func(dialect string) error {
+		return fmt.Errorf("nproto.npmsg.dbstore.DBStore: Unknown dialect: %+q", dialect)
+	}
 )
 
-type DBMsgStore struct {
-	// Options.
-	logger    zerolog.Logger
-	retryWait time.Duration
-	flushWait time.Duration
-	batch     int
+type DBStore struct {
+	logger         zerolog.Logger
+	deleteBulkSize int
+	maxInflight    int
+	maxBuf         int
+	createTable    bool
 
 	// Immutable fields.
-	downstream npmsg.RawMsgPublisher
+	downstream npmsg.RawMsgAsyncPublisher
+	dialect    dbStoreDialect
 	db         *sql.DB
-	dialect    DBMsgStoreDialect
 	table      string
 
 	// Mutable fields.
@@ -41,41 +44,16 @@ type DBMsgStore struct {
 	closeFunc context.CancelFunc
 }
 
-type DBRawMsgPublisher struct {
+type DBPublisher struct {
 	// Immutable fields.
-	store *DBMsgStore
+	store *DBStore
 	q     Queryer
+	batch string // Batch id.
 
 	// Mutable fields.
-	mu       sync.Mutex
-	ids      []string
-	subjects []string
-	datas    [][]byte
-}
-
-type DBMsgStoreDialect interface {
-	// InsertMsg is used to store a message.
-	InsertMsg(ctx context.Context, q Queryer, table string, id, subject string, data []byte) error
-
-	// DeleteMsgs is used to delete published messages.
-	DeleteMsgs(ctx context.Context, q Queryer, table string, ids []string) error
-
-	// CreateMsgStoreTable creates the message store table if not exists.
-	CreateMsgStoreTable(ctx context.Context, q Queryer, table string) error
-
-	// GetLock is used to acquire an global lock on the given table.
-	GetLock(ctx context.Context, conn *sql.Conn, table string) (acquired bool, err error)
-
-	// ReleaseLock is used to releases the lock acquired by GetLock.
-	ReleaseLock(ctx context.Context, conn *sql.Conn, table string) error
-
-	// SelectMsgs is used to select all messages older than window.
-	// A message is returned for each call to iter(true), id == "" means there is no more messages or an error occurred.
-	// Use iter(false) to close the iterator.
-	SelectMsgs(ctx context.Context, conn *sql.Conn, table string, window time.Duration) (
-		iter func(next bool) (id, subject string, data []byte, err error),
-		err error,
-	)
+	mu      sync.Mutex
+	n       int      // Number of published messages.
+	bufMsgs *msgList // Buffered messages, no more than maxBuf.
 }
 
 // Queryer abstracts sql.DB/sql.Conn/sql.Tx .
@@ -85,261 +63,318 @@ type Queryer interface {
 	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
 }
 
-// Option is option in creating DBMsgStore.
-type Option func(*DBMsgStore) error
+type dbStoreDialect interface {
+	CreateSQL(table string) string
+	InsertMsg(ctx context.Context, q Queryer, table, batch, subject string, data []byte) (id int64, err error)
+	DeleteMsgs(ctx context.Context, q Queryer, table string, ids []int64) error
+	SelectMsgsByBatch(ctx context.Context, q Queryer, table, batch string) msgStream
+}
+
+type Option func(*DBStore) error
 
 var (
-	_ npmsg.RawMsgPublisher = (*DBRawMsgPublisher)(nil)
+	_ npmsg.RawMsgPublisher = (*DBPublisher)(nil)
 )
 
-// NewDBMsgStore creates a new DBMsgStore.
-func NewDBMsgStore(downstream npmsg.RawMsgPublisher, db *sql.DB, dialect DBMsgStoreDialect, table string, opts ...Option) (*DBMsgStore, error) {
-	store := &DBMsgStore{
-		logger:     zerolog.Nop(),
-		retryWait:  DefaultRetryWait,
-		flushWait:  DefaultFlushWait,
-		batch:      DefaultBatch,
-		downstream: downstream,
-		db:         db,
-		dialect:    dialect,
-		table:      table,
+func NewDBStore(downstream npmsg.RawMsgAsyncPublisher, dialect string, db *sql.DB, table string, opts ...Option) (*DBStore, error) {
+	ret := &DBStore{
+		logger:         zerolog.Nop(),
+		deleteBulkSize: DefaultDeleteBulkSize,
+		maxInflight:    DefaultMaxInflight,
+		maxBuf:         DefaultMaxBuf,
+		downstream:     downstream,
+		db:             db,
+		table:          table,
 	}
-	store.closeCtx, store.closeFunc = context.WithCancel(context.Background())
+
+	switch dialect {
+	case "mysql":
+		ret.dialect = mysqlDialect{}
+	default:
+		return nil, ErrUnknownDialect(dialect)
+	}
+
 	for _, opt := range opts {
-		if err := opt(store); err != nil {
+		if err := opt(ret); err != nil {
 			return nil, err
 		}
 	}
 
-	if err := dialect.CreateMsgStoreTable(context.Background(), db, table); err != nil {
-		return nil, err
+	if ret.maxBuf > ret.maxInflight {
+		return nil, ErrMaxInflightAndBuf
 	}
 
-	store.loop()
-	return store, nil
-}
-
-func (store *DBMsgStore) NewPublisher(q Queryer) *DBRawMsgPublisher {
-	return &DBRawMsgPublisher{
-		store: store,
-		q:     q,
+	if ret.createTable {
+		_, err := db.Exec(ret.dialect.CreateSQL(table))
+		if err != nil {
+			return nil, err
+		}
 	}
+
+	ret.closeCtx, ret.closeFunc = context.WithCancel(context.Background())
+	// TODO: launch re-delivery goroutine.
+
+	return ret, nil
 }
 
-func (store *DBMsgStore) Close() {
+func (store *DBStore) Close() {
 	store.closeFunc()
+	// TODO:
 }
 
-func (store *DBMsgStore) loop() {
-	fn := func() {
-		// Wait for connection.
-		conn, err := store.getConn()
-		if err != nil {
-			return
-		}
-		defer conn.Close()
+func (store *DBStore) NewPublisher(q Queryer) *DBPublisher {
 
-		// Wait for global lock.
-		err = store.getLock(conn)
-		if err != nil {
-			return
-		}
-		defer store.dialect.ReleaseLock(store.closeCtx, conn, store.table)
-
-		// Main loop.
-		for {
-			err = store.flushAll(conn)
-			if err != nil {
-				return
-			}
-
-			select {
-			case <-store.closeCtx.Done():
-				return
-			case <-time.After(store.flushWait):
-				continue
-			}
-		}
-
-	}
-	go func() {
-		// Loop forever until closeCtx is done.
-		for {
-			select {
-			case <-store.closeCtx.Done():
-				return
-			default:
-			}
-			fn()
-		}
-	}()
-}
-
-// getConn gets a single connection to db. It returns an error when closed.
-func (store *DBMsgStore) getConn() (conn *sql.Conn, err error) {
-	logger := store.logger.With().Str("fn", "getConn").Logger()
-	for {
-		conn, err = store.db.Conn(store.closeCtx)
-		if err == nil {
-			return
-		}
-		logger.Error().Err(err).Msg("")
-
-		select {
-		case <-store.closeCtx.Done():
-			return nil, store.closeCtx.Err()
-		case <-time.After(store.retryWait):
-			continue
-		}
+	return &DBPublisher{
+		store:   store,
+		q:       q,
+		batch:   xid.New().String(),
+		bufMsgs: &msgList{},
 	}
 }
 
-// getLock gets a global lock. It returns nil when lock acquired.
-// And returns an error when DBMsgStoreDialect.GetLock returns error or closed.
-func (store *DBMsgStore) getLock(conn *sql.Conn) (err error) {
-	logger := store.logger.With().Str("fn", "getLock").Logger()
-	for {
-		acquired, err := store.dialect.GetLock(store.closeCtx, conn, store.table)
-		if acquired {
-			return nil
-		}
-		if err != nil {
-			logger.Error().Err(err).Msg("")
-			return err
-		}
-
-		select {
-		case <-store.closeCtx.Done():
-			return store.closeCtx.Err()
-		case <-time.After(store.retryWait):
-			continue
-		}
-	}
-}
-
-// flushAll flushes all messages to downstream. It returns error when db op error or closed.
-func (store *DBMsgStore) flushAll(conn *sql.Conn) (err error) {
-	logger := store.logger.With().Str("fn", "flushAll").Logger()
-
-	iter, err := store.dialect.SelectMsgs(store.closeCtx, conn, store.table, store.flushWait)
-	if err != nil {
-		logger.Error().Err(err).Msg("SelectMsgs error")
-		return err
-	}
-	defer iter(false)
-
-	ids := make([]string, 0)
-	subjects := make([]string, 0)
-	datas := make([][]byte, 0)
-	for {
-		// Reuse buffers.
-		ids = ids[:0]
-		subjects = subjects[:0]
-		datas := datas[:0]
-
-		// Collect no more than batch messages a time.
-		for len(ids) < store.batch {
-			id, subject, data, err := iter(true)
-			if err != nil {
-				logger.Error().Err(err).Msg("iter error")
-				return err
-			}
-			if id == "" {
-				break
-			}
-			ids = append(ids, id)
-			subjects = append(subjects, subject)
-			datas = append(datas, data)
-		}
-
-		// No message.
-		if len(ids) == 0 {
-			return nil
-		}
-
-		// Flush to downstream.
-		pubErrs, err := store.flush(store.closeCtx, ids, subjects, datas)
-
-		// NOTE: Log only one publish error.
-		for _, pubErr := range pubErrs {
-			if pubErr != nil {
-				logger.Error().Err(pubErr).Msg("publish error")
-				break
-			}
-		}
-		if err != nil {
-			logger.Error().Err(err).Msg("flush error")
-			return err
-		}
-	}
-
-}
-
-// flush publishes a batch of messages to downstream.
-func (store *DBMsgStore) flush(ctx context.Context, ids, subjects []string, datas [][]byte) (errs []error, err error) {
-	if len(ids) == 0 {
+func (store *DBStore) flushMsgList(ctx context.Context, list *msgList) {
+	if list.n == 0 {
 		return
 	}
+	logger := store.logger.With().Str("fn", "flushMsgList").Logger()
 
-	// Publish to downstream.
-	switch downstream := store.downstream.(type) {
-	case npmsg.RawBatchMsgPublisher:
-		errs = downstream.PublishBatch(ctx, subjects, datas)
-	default:
-		// Fallback to use a loop.
-		for i, subject := range subjects {
-			errs = append(errs, downstream.Publish(ctx, subject, datas[i]))
+	// Publish loop.
+	pubwg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+	succList := msgList{}
+L:
+	for {
+		// Context maybe done during publishing.
+		select {
+		case <-ctx.Done():
+			logger.Warn().Msg("context done during publishing")
+			break L
+		default:
+		}
+
+		// Pop msg.
+		msg := list.Pop()
+		if msg == nil {
+			break
+		}
+
+		// Add publish task.
+		pubwg.Add(1)
+		cb := func(err error) {
+			if err != nil {
+				// Release the msg if err.
+				logger.Error().Err(err).Msg("publish error")
+				deleteNode(msg)
+			} else {
+				// Append to success list.
+				mu.Lock()
+				succList.Append(msg)
+				mu.Unlock()
+			}
+			// Publish task done.
+			pubwg.Done()
+		}
+
+		// PublishAsync.
+		if err := store.downstream.PublishAsync(ctx, msg.Subject, msg.Data, cb); err != nil {
+			cb(err)
 		}
 	}
 
-	// Collect published messages' ids.
-	doneIds := []string{}
-	for i, err := range errs {
-		if err == nil {
-			doneIds = append(doneIds, ids[i])
-		}
-	}
+	// Wait all publish done.
+	pubwg.Wait()
 
-	// Delete published messages.
-	if len(doneIds) != 0 {
-		err = store.dialect.DeleteMsgs(ctx, store.db, store.table, doneIds)
-	}
-	return
+	// Delete.
+	store.deleteMsgs(&succList, nil, nil)
+
+	// Cleanup.
+	list.Reset()
+	succList.Reset()
 }
 
-func (p *DBRawMsgPublisher) Publish(ctx context.Context, subject string, data []byte) error {
-	// Save to db for reliable.
-	id := xid.New().String()
-	if err := p.store.dialect.InsertMsg(ctx, p.q, p.store.table, id, subject, data); err != nil {
+func (store *DBStore) flushMsgStream(ctx context.Context, stream msgStream) {
+	logger := store.logger.With().Str("fn", "flushMsgStream").Logger()
+
+	// This channel is used to control flush speed.
+	taskc := make(chan struct{}, store.maxInflight)
+
+	// This channel is used to sync with delete goroutine.
+	delc := make(chan bool, 1)
+
+	mu := &sync.Mutex{}
+	succList := &msgList{}
+	procList := &msgList{}
+
+	// Start a seperate goroutine for deleting msgs.
+	delwg := &sync.WaitGroup{}
+	delwg.Add(1)
+	go func() {
+		ok := true
+		idsBuff := []int64{} // Reusable buffer.
+		for ok {
+			// NOTE: When delc is closed, ok will become false.
+			// But we still need to run one more time.
+			ok = <-delc
+
+			// Swap succList and procList.
+			mu.Lock()
+			tmp := succList
+			succList = procList
+			procList = tmp
+			mu.Unlock()
+
+			// Delete msgs in procList.
+			store.deleteMsgs(procList, idsBuff, taskc)
+
+			// Cleanup.
+			procList.Reset()
+		}
+		delwg.Done()
+	}()
+
+	// Publish loop.
+	pubwg := &sync.WaitGroup{}
+L:
+	for {
+		// Context maybe done during publishing.
+		select {
+		case <-ctx.Done():
+			logger.Warn().Msg("context done during publishing")
+			break L
+		case taskc <- struct{}{}:
+		}
+
+		// Get msg.
+		msg, err := stream(true)
+		if msg == nil {
+			if err != nil {
+				logger.Error().Err(err).Msg("msg stream error")
+			}
+			<-taskc
+			break
+		}
+
+		// Add publish task.
+		pubwg.Add(1)
+		cb := func(err error) {
+			if err != nil {
+				// Release the msg if err.
+				<-taskc
+				logger.Error().Err(err).Msg("publish error")
+				deleteNode(msg)
+			} else {
+				// Append to success list.
+				mu.Lock()
+				succList.Append(msg)
+				mu.Unlock()
+				// Kick delete goroutine in non-blocking manner.
+				select {
+				case delc <- true:
+				default:
+				}
+			}
+			// Publish task done.
+			pubwg.Done()
+		}
+
+		// PublishAsync.
+		if err := store.downstream.PublishAsync(ctx, msg.Subject, msg.Data, cb); err != nil {
+			cb(err)
+		}
+	}
+
+	// Close the stream.
+	stream(false)
+
+	// Wait all publish done.
+	pubwg.Wait()
+
+	// Close delc and wait delete goroutine end.
+	close(delc)
+	delwg.Wait()
+
+	// Cleanup.
+	succList.Reset()
+	procList.Reset()
+}
+
+func (store *DBStore) deleteMsgs(list *msgList, idsBuff []int64, taskc chan struct{}) {
+	if list.n == 0 {
+		return
+	}
+	iter := list.Iterate()
+	end := false
+	for !end {
+		// Collect no more than deleteBulkSize message ids.
+		idsBuff = idsBuff[0:0]
+		for len(idsBuff) < store.deleteBulkSize {
+			msg := iter()
+			if msg == nil {
+				end = true
+				break
+			}
+			idsBuff = append(idsBuff, msg.Id)
+		}
+
+		// Delete.
+		if err := store.dialect.DeleteMsgs(context.Background(), store.db, store.table, idsBuff); err != nil {
+			store.logger.Error().Err(err).Str("fn", "deleteMsgs").Msg("delete msg error")
+		}
+
+		// If there is a task channel, finish them.
+		if taskc != nil {
+			for _ = range idsBuff {
+				<-taskc
+			}
+		}
+	}
+}
+
+func (p *DBPublisher) Publish(ctx context.Context, subject string, data []byte) error {
+	// First save to db.
+	store := p.store
+	id, err := store.dialect.InsertMsg(ctx, p.q, store.table, p.batch, subject, data)
+	if err != nil {
 		return err
 	}
 
-	// Save to internal buffer.
+	// Append to bufMsgs if not more then maxBuf.
 	p.mu.Lock()
-	p.ids = append(p.ids, id)
-	p.subjects = append(p.subjects, subject)
-	p.datas = append(p.datas, data)
+	if p.n < store.maxBuf {
+		msg := newNode()
+		msg.Id = id
+		msg.Subject = subject
+		msg.Data = data
+		p.bufMsgs.Append(msg)
+	}
+	p.n += 1
 	p.mu.Unlock()
 	return nil
 }
 
-func (p *DBRawMsgPublisher) Flush(ctx context.Context) {
-	// Get buffered messages.
+func (p *DBPublisher) Finish(ctx context.Context, committed bool) {
 	p.mu.Lock()
-	ids := p.ids
-	subjects := p.subjects
-	datas := p.datas
-	p.ids = nil
-	p.subjects = nil
-	p.datas = nil
+	n := p.n
+	bufMsgs := p.bufMsgs
+	p.n = 0
+	p.bufMsgs = nil // Just set to nil since there should be no further Publish.
 	p.mu.Unlock()
+	defer bufMsgs.Reset()
 
-	// Nothing to do.
-	if len(ids) == 0 {
+	// Do nothing if rollbacked.
+	if !committed {
 		return
 	}
 
-	// Flush to downstream.
-	p.store.flush(ctx, ids, subjects, datas)
-	return
+	store := p.store
+	// Small amount of messages. We can use the buffered messages directly.
+	// Also no need to query db again.
+	if bufMsgs.n == n {
+		store.flushMsgList(ctx, bufMsgs)
+		return
+	}
+
+	// For larger amount of messages, we need to query the db again.
+	store.flushMsgStream(ctx, store.dialect.SelectMsgsByBatch(ctx, store.db, store.table, p.batch))
+
 }
--- npmsg/dbstore/options.go
+++ npmsg/dbstore/options.go
@@ -1,39 +1,52 @@
 package dbstore
 
 import (
-	"time"
+	"fmt"
 
 	"github.com/rs/zerolog"
 )
 
 // OptLogger sets structured logger.
 func OptLogger(logger *zerolog.Logger) Option {
-	return func(store *DBMsgStore) error {
-		store.logger = logger.With().Str("component", "nproto.npmsg.dbstore.DBMsgStore").Logger()
+	return func(store *DBStore) error {
+		store.logger = logger.With().Str("component", "nproto.npmsg.dbstore.DBStore").Logger()
 		return nil
 	}
 }
 
-// OptRetryWait sets the interval between db reconnections and lock acquiring.
-func OptRetryWait(t time.Duration) Option {
-	return func(store *DBMsgStore) error {
-		store.retryWait = t
+func OptDeleteBulkSize(bulkSize int) Option {
+	return func(store *DBStore) error {
+		if bulkSize <= 0 {
+			return fmt.Errorf("nproto.npmsg.dbstore.DBStore: DeleteBulkSize must >= 1")
+		}
+		store.deleteBulkSize = bulkSize
 		return nil
 	}
 }
 
-// OptFlushWait sets the interval between flushing messages to downstream.
-func OptFlushWait(t time.Duration) Option {
-	return func(store *DBMsgStore) error {
-		store.flushWait = t
+func OptMaxInflight(maxInflight int) Option {
+	return func(store *DBStore) error {
+		if maxInflight <= 0 {
+			return fmt.Errorf("nproto.npmsg.dbstore.DBStore: MaxInflight must >= 1")
+		}
+		store.maxInflight = maxInflight
 		return nil
 	}
 }
 
-// OptBatch sets the number of messages inflight (not yet ack) when flushing to downstream.
-func OptBatch(batch int) Option {
-	return func(store *DBMsgStore) error {
-		store.batch = batch
+func OptMaxBuf(maxBuf int) Option {
+	return func(store *DBStore) error {
+		if maxBuf <= 0 {
+			return fmt.Errorf("nproto.npmsg.dbstore.DBStore: MaxBuf must >= 1")
+		}
+		store.maxBuf = maxBuf
+		return nil
+	}
+}
+
+func OptCreateTable() Option {
+	return func(store *DBStore) error {
+		store.createTable = true
 		return nil
 	}
 }
