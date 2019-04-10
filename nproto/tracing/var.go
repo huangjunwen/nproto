0a2aed249b95bf32b81022bd26dac2e3f49aebbd
jayven <kassarar@gmail.com>
2019-04-10 09:40:55 +0800

    Change tracing op name

--- nproto/tracing/var.go
+++ nproto/tracing/var.go
@@ -19,22 +19,22 @@ var (
 	TracingMDKey = "NProtoTracing"
 	// ClientHandlerOpNameFmt is used to generate operation name of a RPC client handler span.
 	ClientHandlerOpNameFmt = func(svcName string, method *nproto.RPCMethod) string {
-		return fmt.Sprintf("ClientHandler::%s::%s", svcName, method.Name)
+		return fmt.Sprintf("RPC Client %s:%s", svcName, method.Name)
 	}
 	// ServerHandlerOpNameFmt is used to generate operation name of a RPC server handler span.
 	ServerHandlerOpNameFmt = func(svcName string, method *nproto.RPCMethod) string {
-		return fmt.Sprintf("ServerHandler::%s::%s", svcName, method.Name)
+		return fmt.Sprintf("RPC Server %s:%s", svcName, method.Name)
 	}
 	// PublishOpNameFmt is used to generate operation name of a msg publish span.
 	PublishOpNameFmt = func(subject string) string {
-		return fmt.Sprintf("Publish::%s", subject)
+		return fmt.Sprintf("Msg Publisher %s", subject)
 	}
 	// PublishAsyncOpNameFmt is used to generate operation name of a async msg publish span.
 	PublishAsyncOpNameFmt = func(subject string) string {
-		return fmt.Sprintf("PublishAsync::%s", subject)
+		return fmt.Sprintf("Msg PublishAsync %s", subject)
 	}
 	// SubscriberHandlerOpNameFmt is used to generate operation name of a subscription handler span.
 	SubscriberHandlerOpNameFmt = func(subject, queue string) string {
-		return fmt.Sprintf("SubscriberHandler::%s::%s", subject, queue)
+		return fmt.Sprintf("Msg Subscriber %s:%s", subject, queue)
 	}
 )
