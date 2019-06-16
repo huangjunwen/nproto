9290f6730d68dfe294dbdc86ce82eb660b3f2a5f
jayven <kassarar@gmail.com>
2019-06-16 12:11:09 +0800

    Add version info

--- protoc-gen-nproto/main.go
+++ protoc-gen-nproto/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"runtime/debug"
+
 	pgs "github.com/lyft/protoc-gen-star"
 )
 
 func main() {
+	if len(os.Args) == 2 && os.Args[1] == "version" {
+		info, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("protoc-gen-nproto version=v? sum=?")
+		} else {
+			fmt.Printf("protoc-gen-nproto version=%s sum=%s\n", info.Main.Version, info.Main.Sum)
+		}
+		return
+	}
 	pgs.Init().RegisterModule(
 		&nprotoModule{},
 	).RegisterPostProcessor(
