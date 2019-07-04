7baaa2b803383100a0d7fea9e1439ecf6d1acdb6
jayven <kassarar@gmail.com>
2019-07-04 17:09:16 +0800

    Using new middleware function to implement tracing support

--- nproto/tracing/msg.go
+++ nproto/tracing/msg.go
@@ -3,247 +3,178 @@ package tracing
 import (
 	"context"
 
+	_ "github.com/golang/protobuf/proto"
 	ot "github.com/opentracing/opentracing-go"
 	otext "github.com/opentracing/opentracing-go/ext"
 
 	"github.com/huangjunwen/nproto/nproto"
 )
 
-// TracedMsgPublisher wraps nproto.MsgPublisher to support opentracing.
-type TracedMsgPublisher struct {
-	publisher  nproto.MsgPublisher
-	tracer     ot.Tracer
-	downstream bool
-}
+// TracedMsgPublisher adds opentracing support for MsgPublisher.
+func TracedMsgPublisher(tracer ot.Tracer, downstream bool) nproto.MsgPublisherMiddleware {
+	return func(next nproto.MsgPublisherFunc) nproto.MsgPublisherFunc {
+		return func(ctx context.Context, subject string, msgData []byte) (err error) {
+			var (
+				parentSpanCtx ot.SpanContext
+				spanRef       ot.SpanReference
+			)
 
-// TracedMsgAsyncPublisher wraps nproto.MsgAsyncPublisher to support opentracing.
-type TracedMsgAsyncPublisher TracedMsgPublisher
+			// Gets parent span reference.
+			if downstream {
+				parentSpanCtx, err = extractSpanCtx(
+					tracer,
+					nproto.MDFromOutgoingContext(ctx),
+				)
+				if err != nil {
+					return
+				}
+				if parentSpanCtx != nil {
+					spanRef = ot.FollowsFrom(parentSpanCtx)
+				}
+			} else {
+				parentSpanCtx = spanCtxFromCtx(ctx)
+				if parentSpanCtx != nil {
+					spanRef = ot.ChildOf(parentSpanCtx)
+				}
+			}
 
-// TracedMsgSubscriber wraps nproto.MsgSubscriber to support opentracing.
-type TracedMsgSubscriber struct {
-	subscriber nproto.MsgSubscriber
-	tracer     ot.Tracer
-}
+			// Do not start new span if no parent span.
+			if parentSpanCtx == nil {
+				err = next(ctx, subject, msgData)
+				return
+			}
 
-var (
-	_ nproto.MsgPublisher      = (*TracedMsgPublisher)(nil)
-	_ nproto.MsgAsyncPublisher = (*TracedMsgAsyncPublisher)(nil)
-	_ nproto.MsgSubscriber     = (*TracedMsgSubscriber)(nil)
-)
+			// Starts a producer span.
+			span := tracer.StartSpan(
+				PublishOpNameFmt(subject),
+				spanRef,
+				otext.SpanKindProducer,
+				ComponentTag,
+			)
+			defer func() {
+				setSpanError(span, err)
+				span.Finish()
+			}()
 
-// NewTracedMsgPublisher wraps a nproto.MsgPublisher to TracedMsgPublisher.
-func NewTracedMsgPublisher(publisher nproto.MsgPublisher, tracer ot.Tracer) *TracedMsgPublisher {
-	return &TracedMsgPublisher{
-		publisher: publisher,
-		tracer:    tracer,
-	}
-}
+			// Injects span context.
+			md := nproto.MDFromOutgoingContext(ctx)
+			md, err = injectSpanCtx(tracer, span.Context(), md)
+			if err != nil {
+				return
+			}
+			ctx = nproto.NewOutgoingContextWithMD(ctx, md)
 
-// NewDownstreamTracedMsgPublisher wraps a nproto.MsgPublisher to TracedMsgPublisher. It is used in pipeline downstream.
-func NewDownstreamTracedMsgPublisher(publisher nproto.MsgPublisher, tracer ot.Tracer) *TracedMsgPublisher {
-	return &TracedMsgPublisher{
-		publisher:  publisher,
-		tracer:     tracer,
-		downstream: true,
-	}
-}
-
-// NewTracedMsgAsyncPublisher wraps a nproto.MsgAsyncPublisher to TracedMsgAsyncPublisher.
-func NewTracedMsgAsyncPublisher(publisher nproto.MsgAsyncPublisher, tracer ot.Tracer) *TracedMsgAsyncPublisher {
-	return &TracedMsgAsyncPublisher{
-		publisher: publisher,
-		tracer:    tracer,
-	}
-}
-
-// NewDownstreamTracedMsgAsyncPublisher wraps a nproto.MsgAsyncPublisher to TracedMsgAsyncPublisher. It is used in pipeline downstream.
-func NewDownstreamTracedMsgAsyncPublisher(publisher nproto.MsgAsyncPublisher, tracer ot.Tracer) *TracedMsgAsyncPublisher {
-	return &TracedMsgAsyncPublisher{
-		publisher:  publisher,
-		tracer:     tracer,
-		downstream: true,
-	}
-}
-
-// Publish implements nproto.MsgPublisher interface.
-func (publisher *TracedMsgPublisher) Publish(ctx context.Context, subject string, msgData []byte) (err error) {
-
-	tracer := publisher.tracer
-	var (
-		parentSpanCtx ot.SpanContext
-		spanRef       ot.SpanReference
-	)
-
-	// Gets parent span reference.
-	if publisher.downstream {
-		parentSpanCtx, err = extractSpanCtx(
-			tracer,
-			nproto.MDFromOutgoingContext(ctx),
-		)
-		if err != nil {
+			// Publish.
+			err = next(ctx, subject, msgData)
 			return
 		}
-		if parentSpanCtx != nil {
-			spanRef = ot.FollowsFrom(parentSpanCtx)
-		}
-	} else {
-		parentSpanCtx = spanCtxFromCtx(ctx)
-		if parentSpanCtx != nil {
-			spanRef = ot.ChildOf(parentSpanCtx)
-		}
 	}
-
-	// Do not start new span if no parent span.
-	if parentSpanCtx == nil {
-		err = publisher.publisher.Publish(ctx, subject, msgData)
-		return
-	}
-
-	// Starts a producer span.
-	span := tracer.StartSpan(
-		PublishOpNameFmt(subject),
-		spanRef,
-		otext.SpanKindProducer,
-		ComponentTag,
-	)
-	defer func() {
-		setSpanError(span, err)
-		span.Finish()
-	}()
-
-	// Injects span context.
-	md := nproto.MDFromOutgoingContext(ctx)
-	md, err = injectSpanCtx(tracer, span.Context(), md)
-	if err != nil {
-		return
-	}
-	ctx = nproto.NewOutgoingContextWithMD(ctx, md)
-
-	// Publish.
-	err = publisher.publisher.Publish(ctx, subject, msgData)
-	return
 }
 
-// Publish implements nproto.MsgAsyncPublisher interface.
-func (publisher *TracedMsgAsyncPublisher) Publish(ctx context.Context, subject string, msgData []byte) error {
-	return (*TracedMsgPublisher)(publisher).Publish(ctx, subject, msgData)
-}
+// TracedMsgAsyncPublisher adds opentracing support for MsgAsyncPublisher.
+func TracedMsgAsyncPublisher(tracer ot.Tracer, downstream bool) nproto.MsgAsyncPublisherMiddleware {
+	return func(next nproto.MsgAsyncPublisherFunc) nproto.MsgAsyncPublisherFunc {
+		return func(ctx context.Context, subject string, msgData []byte, cb func(error)) (err error) {
+			var (
+				parentSpanCtx ot.SpanContext
+				spanRef       ot.SpanReference
+			)
 
-// PublishAsync implements nproto.MsgAsyncPublisher interface.
-func (publisher *TracedMsgAsyncPublisher) PublishAsync(ctx context.Context, subject string, msgData []byte, cb func(error)) (err error) {
+			// Gets parent span reference.
+			if downstream {
+				parentSpanCtx, err = extractSpanCtx(
+					tracer,
+					nproto.MDFromOutgoingContext(ctx),
+				)
+				if err != nil {
+					return
+				}
+				if parentSpanCtx != nil {
+					spanRef = ot.FollowsFrom(parentSpanCtx)
+				}
+			} else {
+				parentSpanCtx = spanCtxFromCtx(ctx)
+				if parentSpanCtx != nil {
+					spanRef = ot.FollowsFrom(parentSpanCtx) // Use FollowFrom since PublishAsync is async op.
+				}
+			}
 
-	tracer := publisher.tracer
-	var (
-		parentSpanCtx ot.SpanContext
-		spanRef       ot.SpanReference
-	)
-	p := publisher.publisher.(nproto.MsgAsyncPublisher)
+			// Do not start new span if no parent span.
+			if parentSpanCtx == nil {
+				err = next(ctx, subject, msgData, cb)
+				return
+			}
 
-	// Gets parent span reference.
-	if publisher.downstream {
-		parentSpanCtx, err = extractSpanCtx(
-			tracer,
-			nproto.MDFromOutgoingContext(ctx),
-		)
-		if err != nil {
+			// Starts a producer span.
+			span := tracer.StartSpan(
+				PublishAsyncOpNameFmt(subject),
+				spanRef,
+				otext.SpanKindProducer,
+				ComponentTag,
+			)
+			fin := func(err error) {
+				setSpanError(span, err)
+				span.Finish()
+			}
+
+			// Injects span context.
+			md := nproto.MDFromOutgoingContext(ctx)
+			md, err = injectSpanCtx(tracer, span.Context(), md)
+			if err != nil {
+				fin(err)
+				return
+			}
+			ctx = nproto.NewOutgoingContextWithMD(ctx, md)
+
+			// PublishAsync.
+			if err = next(ctx, subject, msgData, func(cbErr error) {
+				cb(cbErr)
+				fin(cbErr)
+			}); err != nil {
+				fin(err)
+			}
 			return
 		}
-		if parentSpanCtx != nil {
-			spanRef = ot.FollowsFrom(parentSpanCtx)
-		}
-	} else {
-		parentSpanCtx = spanCtxFromCtx(ctx)
-		if parentSpanCtx != nil {
-			spanRef = ot.FollowsFrom(parentSpanCtx) // Use FollowFrom since PublishAsync is async op.
-		}
-	}
-
-	// Do not start new span if no parent span.
-	if parentSpanCtx == nil {
-		err = p.PublishAsync(ctx, subject, msgData, cb)
-		return
-	}
-
-	// Starts a producer span.
-	span := tracer.StartSpan(
-		PublishAsyncOpNameFmt(subject),
-		spanRef,
-		otext.SpanKindProducer,
-		ComponentTag,
-	)
-	fin := func(err error) {
-		setSpanError(span, err)
-		span.Finish()
-	}
-
-	// Injects span context.
-	md := nproto.MDFromOutgoingContext(ctx)
-	md, err = injectSpanCtx(tracer, span.Context(), md)
-	if err != nil {
-		fin(err)
-		return
-	}
-	ctx = nproto.NewOutgoingContextWithMD(ctx, md)
-
-	// PublishAsync.
-	if err = p.PublishAsync(ctx, subject, msgData, func(cbErr error) {
-		cb(cbErr)
-		fin(cbErr)
-	}); err != nil {
-		fin(err)
-	}
-	return
-}
-
-// NewTracedMsgSubscriber wraps a nproto.MsgSubscriber to TracedMsgSubscriber.
-func NewTracedMsgSubscriber(subscriber nproto.MsgSubscriber, tracer ot.Tracer) *TracedMsgSubscriber {
-	return &TracedMsgSubscriber{
-		subscriber: subscriber,
-		tracer:     tracer,
 	}
 }
 
-// Subscribe implements nproto.MsgSubscriber interface.
-func (subscriber *TracedMsgSubscriber) Subscribe(subject string, queue string, handler nproto.MsgHandler, opts ...interface{}) error {
+func TracedMsgSubscriber(tracer ot.Tracer) nproto.MsgMiddleware {
+	return func(subject, queue string, handler nproto.MsgHandler) nproto.MsgHandler {
+		opName := SubscriberHandlerOpNameFmt(subject, queue)
+		return func(ctx context.Context, msgData []byte) (err error) {
+			// Gets parent span context.
+			parentSpanCtx, err := extractSpanCtx(
+				tracer,
+				nproto.MDFromIncomingContext(ctx),
+			)
+			if err != nil {
+				return
+			}
 
-	tracer := subscriber.tracer
-	opName := SubscriberHandlerOpNameFmt(subject, queue)
+			// Do not start new span if no parent span.
+			if parentSpanCtx == nil {
+				err = handler(ctx, msgData)
+				return
+			}
 
-	handler2 := func(ctx context.Context, msgData []byte) (err error) {
-		// Gets parent span context.
-		parentSpanCtx, err := extractSpanCtx(
-			tracer,
-			nproto.MDFromIncomingContext(ctx),
-		)
-		if err != nil {
-			return
-		}
+			// Starts a consumer span.
+			span := tracer.StartSpan(
+				opName,
+				ot.FollowsFrom(parentSpanCtx),
+				otext.SpanKindConsumer,
+				ComponentTag,
+			)
+			defer func() {
+				setSpanError(span, err)
+				span.Finish()
+			}()
 
-		// Do not start new span if no parent span.
-		if parentSpanCtx == nil {
+			// Adds span to context.
+			ctx = ot.ContextWithSpan(ctx, span)
+
+			// Handles.
 			err = handler(ctx, msgData)
 			return
 		}
-
-		// Starts a consumer span.
-		span := tracer.StartSpan(
-			opName,
-			ot.FollowsFrom(parentSpanCtx),
-			otext.SpanKindConsumer,
-			ComponentTag,
-		)
-		defer func() {
-			setSpanError(span, err)
-			span.Finish()
-		}()
-
-		// Adds span to context.
-		ctx = ot.ContextWithSpan(ctx, span)
-
-		// Handles.
-		err = handler(ctx, msgData)
-		return
 	}
-
-	return subscriber.subscriber.Subscribe(subject, queue, handler2, opts...)
 }
--- nproto/tracing/rpc.go
+++ nproto/tracing/rpc.go
@@ -10,95 +10,52 @@ import (
 	"github.com/huangjunwen/nproto/nproto"
 )
 
-// TracedRPCClient wraps nproto.RPCClient to support opentracing.
-type TracedRPCClient struct {
-	client nproto.RPCClient
-	tracer ot.Tracer
-}
+// TracedRPCClient adds opentracing support for RPCClient.
+func TracedRPCClient(tracer ot.Tracer) nproto.RPCMiddleware {
+	return func(svcName string, method *nproto.RPCMethod, handler nproto.RPCHandler) nproto.RPCHandler {
+		opName := ClientHandlerOpNameFmt(svcName, method)
+		return func(ctx context.Context, input proto.Message) (output proto.Message, err error) {
+			// Gets current span context as parent. Maybe nil.
+			parentSpanCtx := spanCtxFromCtx(ctx)
 
-// TracedRPCServer wraps nproto.RPCServer to support opentracing.
-type TracedRPCServer struct {
-	server nproto.RPCServer
-	tracer ot.Tracer
-}
+			// Do not start new span if no parent span.
+			if parentSpanCtx == nil {
+				output, err = handler(ctx, input)
+				return
+			}
 
-var (
-	_ nproto.RPCServer = (*TracedRPCServer)(nil)
-	_ nproto.RPCClient = (*TracedRPCClient)(nil)
-)
+			// Starts a client span.
+			span := tracer.StartSpan(
+				opName,
+				ot.ChildOf(parentSpanCtx),
+				otext.SpanKindRPCClient,
+				ComponentTag,
+			)
+			defer func() {
+				setSpanError(span, err)
+				span.Finish()
+			}()
 
-// NewTracedRPCClient wraps a nproto.RPCClient to TracedRPCClient.
-func NewTracedRPCClient(client nproto.RPCClient, tracer ot.Tracer) *TracedRPCClient {
-	return &TracedRPCClient{
-		client: client,
-		tracer: tracer,
-	}
-}
+			// Injects span context.
+			md := nproto.MDFromOutgoingContext(ctx)
+			md, err = injectSpanCtx(tracer, span.Context(), md)
+			if err != nil {
+				return
+			}
+			ctx = nproto.NewOutgoingContextWithMD(ctx, md)
 
-// MakeHandler implements nproto.RPCClient interface.
-func (client *TracedRPCClient) MakeHandler(svcName string, method *nproto.RPCMethod) nproto.RPCHandler {
-
-	tracer := client.tracer
-	opName := ClientHandlerOpNameFmt(svcName, method)
-	handler := client.client.MakeHandler(svcName, method)
-
-	return func(ctx context.Context, input proto.Message) (output proto.Message, err error) {
-		// Gets current span context as parent. Maybe nil.
-		parentSpanCtx := spanCtxFromCtx(ctx)
-
-		// Do not start new span if no parent span.
-		if parentSpanCtx == nil {
+			// Handles.
 			output, err = handler(ctx, input)
 			return
 		}
-
-		// Starts a client span.
-		span := tracer.StartSpan(
-			opName,
-			ot.ChildOf(parentSpanCtx),
-			otext.SpanKindRPCClient,
-			ComponentTag,
-		)
-		defer func() {
-			setSpanError(span, err)
-			span.Finish()
-		}()
-
-		// Injects span context.
-		md := nproto.MDFromOutgoingContext(ctx)
-		md, err = injectSpanCtx(tracer, span.Context(), md)
-		if err != nil {
-			return
-		}
-		ctx = nproto.NewOutgoingContextWithMD(ctx, md)
-
-		// Handles.
-		output, err = handler(ctx, input)
-		return
 	}
 }
 
-// NewTracedRPCServer wraps a nproto.RPCServer to TracedRPCServer.
-func NewTracedRPCServer(server nproto.RPCServer, tracer ot.Tracer) *TracedRPCServer {
-	return &TracedRPCServer{
-		server: server,
-		tracer: tracer,
-	}
-}
-
-// RegistSvc implements nproto.RPCServer interface.
-func (server *TracedRPCServer) RegistSvc(svcName string, methods map[*nproto.RPCMethod]nproto.RPCHandler) error {
-
-	tracer := server.tracer
-	methods2 := make(map[*nproto.RPCMethod]nproto.RPCHandler)
-
-	for method, handler := range methods {
-
-		method := method
-		handler := handler
+// TracedRPCServer adds opentracing support for RPCServer.
+func TracedRPCServer(tracer ot.Tracer) nproto.RPCMiddleware {
+	return func(svcName string, method *nproto.RPCMethod, handler nproto.RPCHandler) nproto.RPCHandler {
 		opName := ServerHandlerOpNameFmt(svcName, method)
-
-		methods2[method] = func(ctx context.Context, input proto.Message) (output proto.Message, err error) {
+		return func(ctx context.Context, input proto.Message) (output proto.Message, err error) {
 			// Extracts parent span context from client. Maybe nil.
 			parentSpanCtx, err := extractSpanCtx(tracer, nproto.MDFromIncomingContext(ctx))
 			if err != nil {
@@ -129,15 +86,7 @@ func (server *TracedRPCServer) RegistSvc(svcName string, methods map[*nproto.RPC
 			// Handles.
 			output, err = handler(ctx, input)
 			return
-
 		}
-
 	}
 
-	return server.server.RegistSvc(svcName, methods2)
-}
-
-// DeregistSvc implements nproto.RPCServer interface.
-func (server *TracedRPCServer) DeregistSvc(svcName string) error {
-	return server.server.DeregistSvc(svcName)
 }
