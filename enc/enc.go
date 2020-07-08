1fd00b77d74c7be760ff663ec06c017159e8a64a
jayven <kassarar@gmail.com>
2020-07-09 07:50:49 +0800

    Fix wrong doc

--- enc/enc.go
+++ enc/enc.go
@@ -12,13 +12,13 @@ type Encoder interface {
 	// EncodeData encodes data to w.
 	//
 	// If data's type is *RawData/RawData and data.EncoderName == Encoder.EncoderName(),
-	// then data.Data should be written to w directly without encoding.
+	// then data.Bytes should be written to w directly without encoding.
 	EncodeData(w io.Writer, data interface{}) error
 
 	// DecodeData decodes data from r.
 	//
 	// If data's type is *RawData, then data.EncoderName should be set to Encoder.EncoderName(),
-	// and data.Data should be read from r directly without decoding.
+	// and data.Bytes should be read from r directly without decoding.
 	DecodeData(r io.Reader, data interface{}) error
 }
 
