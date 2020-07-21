6b46f19bc994c35b97fcc43878436211f33ded26
jayven <kassarar@gmail.com>
2020-07-21 15:44:26 +0800

    Update some doc.

--- enc/enc.go
+++ enc/enc.go
@@ -1,7 +1,7 @@
 // Package enc contains encode related types.
 package enc
 
-// Encoder is used to encode data.
+// Encoder is used to encode data. Different Encoders may accept different kinds of data and formats.
 type Encoder interface {
 	// EncodeData encodes data.
 	//
@@ -9,7 +9,7 @@ type Encoder interface {
 	EncodeData(data interface{}, targetFormat *string, targetBytes *[]byte) error
 }
 
-// Decoder is used to decode data.
+// Decoder is used to decode data. Different Decoders may accept different kinds of data and formats.
 type Decoder interface {
 	// DecodeData decodes data.
 	DecodeData(srcFormat string, srcBytes []byte, data interface{}) error
